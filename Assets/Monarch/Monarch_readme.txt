888488d292f951e5f42f6d6f64810c18698fd7da
arsenalahm <lizhipeng0603@gmail.com>
2022-08-28 19:02:03 +0800

    Add seahorse

---
+++ Assets/Monarch/Monarch_readme.txt
@@ -0,0 +1,56 @@
+Monarch butterfly 3D model.
+
+Include URP Sample Scene
+15 Animations in root motion
+Added extra folder with In-place animations
+5 Textures (4096x4096)
+3 Materials -URP support-
+1 Shader -URP support-from Two Sided Shaders +URP package 
+(for technical support or buy complete package put in contact with the publisher)
+1 Prefab
+1 Animator Controller
+1 Skybox
+2 Scripts
+1 Scene demo
+
+Low poly model
+2956 quad polygons on this mesh
+3066 vertices on this mesh
+
+Textures
+Monarch_D
+Monarch_Fur_D
+Monarch_N
+Monarch_Wings_D
+Monarch_Wings_N
+
+Animations List
+eat
+fly
+flyfast
+flyinplace
+flyleft
+flyright
+glide
+idle
+idle2
+idle3
+landing
+takeoff
+turnleft
+turnright
+walk
+
+Scene Demo
+keys for enable animations
+
+W=walk/fly
+A=turnleft/flyleft
+D=turnright/flyright
+E=eat
+G=glide
+F=flyfast
+S=flyinplace
+Space=takeoff/landing/fly
+
+enjoy game age:)
---
+++ Assets/Monarch/Scripts/Monarch.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monarch : MonoBehaviour
+{
+    Animator monarch;
+    public GameObject MainCamera;
+    public float gravity = 1.0f;
+    private Vector3 moveDirection = Vector3.zero;
+    CharacterController characterController;
+
+    void Start ()
+    {
+        monarch = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+	void Update ()
+    {
+        characterController.Move(moveDirection * Time.deltaTime);
+        moveDirection.y = gravity * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            monarch.SetBool("glide", true);
+            monarch.SetBool("fly", false);
+        }
+        if (Input.GetKeyUp(KeyCode.G))
+        {
+            monarch.SetBool("fly", true);
+            monarch.SetBool("glide", false);
+        }
+        if ((monarch.GetCurrentAnimatorStateInfo(0).IsName("takeoff"))||(monarch.GetCurrentAnimatorStateInfo(0).IsName("landing")))
+        {
+            monarch.SetBool("takeoff", false);
+            monarch.SetBool("landing", false);
+        }
+        if ((Input.GetKeyUp(KeyCode.W))||(Input.GetKeyUp(KeyCode.A))||(Input.GetKeyUp(KeyCode.D)))
+        {
+            monarch.SetBool("idle", true);
+            monarch.SetBool("walk", false);
+            monarch.SetBool("fly", true);
+            monarch.SetBool("eat", false);
+            monarch.SetBool("turnleft", false);
+            monarch.SetBool("turnright", false);
+            monarch.SetBool("flyleft", false);
+            monarch.SetBool("flyright", false);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            monarch.SetBool("walk", true);
+            monarch.SetBool("idle", false);
+            monarch.SetBool("idle2", false);
+            monarch.SetBool("fly", true);
+            monarch.SetBool("flyinplace", false);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            monarch.SetBool("turnleft", true);
+            monarch.SetBool("flyleft", true);
+            monarch.SetBool("turnright", false);
+            monarch.SetBool("idle", false);
+            monarch.SetBool("idle2", false);
+            monarch.SetBool("walk", false);
+            monarch.SetBool("fly", false);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            monarch.SetBool("turnright", true);
+            monarch.SetBool("flyright", true);
+            monarch.SetBool("turnleft", false);
+            monarch.SetBool("idle", false);
+            monarch.SetBool("idle2", false);
+            monarch.SetBool("walk", false);
+            monarch.SetBool("fly", false);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            monarch.SetBool("takeoff", true);
+            monarch.SetBool("landing", true);
+            monarch.SetBool("idle", false);
+            monarch.SetBool("fly", false);
+            monarch.SetBool("eat", false);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            monarch.SetBool("eat", true);
+            monarch.SetBool("idle", false);
+            monarch.SetBool("idle2", false);
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            monarch.SetBool("eat", false);
+            monarch.SetBool("idle", true);
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            monarch.SetBool("flyfast", true);
+            monarch.SetBool("fly", false);
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            monarch.SetBool("fly", true);
+            monarch.SetBool("flyfast", false);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            monarch.SetBool("flyinplace", true);
+            monarch.SetBool("fly", false);
+        }
+        if (Input.GetKeyDown(KeyCode.RightControl))
+        {
+            MainCamera.GetComponent<CameraFollow>().enabled = false;
+        }
+        if (Input.GetKeyUp(KeyCode.RightControl))
+        {
+            MainCamera.GetComponent<CameraFollow>().enabled = true;
+        }
+
+	}
+}
