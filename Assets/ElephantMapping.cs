dfe4a61a4ab54427ab983a088dfaf2f527b809a3
arsenalahm <lizhipeng0603@gmail.com>
2022-08-31 17:07:06 +0800

    Update elephant

--- Assets/ElephantMapping.cs
+++ Assets/ElephantMapping.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using TMPro;
 
 public class ElephantMapping : MonoBehaviour
 {
     private string mapping_file = "./Assets/elephant_mapping.txt";
     private Dictionary<GameObject, GameObject> mapping = new Dictionary<GameObject, GameObject>();
     private List<string> controlledJoints = new List<string>();
+    private List<string> controlledHandJoints = new List<string>();
     private Dictionary<string, Quaternion> initialRotations = new Dictionary<string, Quaternion>();
     private Dictionary<string, Quaternion> initialHandRotations = new Dictionary<string, Quaternion>();
+    private Dictionary<string, Quaternion> clusterPoseRotations = new Dictionary<string, Quaternion>();
+    private List<float> poseDeviations = new List<float>();
 
     public PlayAnimation player;
     public RecordAvatar recorder;
+
+    public TextMeshProUGUI text;
 
     public string fileName = "test.txt";
     private StreamWriter writer;
@@ -23,6 +29,18 @@ public class ElephantMapping : MonoBehaviour
 
     public GameObject leftHand;
     public GameObject rightHand;
+
+    private bool flag = false;
+    private bool poseFlag = false;
+
+    private float timer = 0f;
+    private float recordTimer = 0f;
+
+    private string clusterFile = "./cluster_poses/elephant_poses.txt";
+    private List<Dictionary<string, List<float>>> clusterPoses = new List<Dictionary<string, List<float>>>();
+    private int clusterPoseCnt = 0;
+
+    public GameObject anotherAvatar;
 
     string ConvertTransformToString(Transform trans)
     {
@@ -78,11 +96,55 @@ public class ElephantMapping : MonoBehaviour
         return char.ToUpper(s[0]) + s.Substring(1);
     }
 
+    void ReadHandJoints() 
+    {        
+        StreamReader reader = new StreamReader(mapping_file);
+        string content = reader.ReadToEnd();
+        reader.Close();
+        string[] pairs = content.Split("\n");
+        foreach (string pair in pairs)
+        {
+            string[] joints = pair.Split(": ");
+            if (joints.Length != 2)
+            {
+                continue;
+            }
+            controlledHandJoints.Add(joints[1]);
+        }
+    }
+
+    void ReadClusterPoses()
+    {
+        StreamReader reader = new StreamReader(clusterFile);
+        string content = reader.ReadToEnd();
+        reader.Close();
+        string[] poses = content.Split("#");
+        foreach (string pose in poses)
+        {
+            string[] pairs = pose.Split("\n");
+            Dictionary<string, List<float>> tt = new Dictionary<string, List<float>>();
+            foreach (string pair in pairs)
+            {
+                string[] data = pair.Split(" ");
+                if (data.Length == 8)
+                {
+                    List<float> t = new List<float>();
+                    for (int i = 1; i < 8; i++)
+                    {
+                        t.Add(float.Parse(data[i]));
+                    }
+                    tt.Add(data[0], t);
+                }
+            }
+            clusterPoses.Add(tt);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        text.text = "Perform the transparent gesture";
         writer = new StreamWriter(fileName);
-
         StreamReader reader = new StreamReader(poseFile);
         string[] content = reader.ReadToEnd().Split("\n");
         foreach (string s in content)
@@ -129,62 +191,176 @@ public class ElephantMapping : MonoBehaviour
         // initialLeftHand.transform.rotation = Quaternion.Euler(0, 180, 0);
         initialRightHand.transform.position = new Vector3(0f, -0.1f, 0.2f);
         // initialRightHand.transform.rotation = Quaternion.Euler(0, 180, 0);
+
+        ReadHandJoints();
+        ReadClusterPoses();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (UnityEngine.Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.M))
         {
-            // OVRSkeleton leftSkeleton = leftHand.GetComponent<OVRSkeleton>();
-            // for (int i = 0; i < leftSkeleton.Bones.Count; i++)
-            // {
-            //     initialHandRotations.Add(leftSkeleton.Bones[i].Transform.name, leftSkeleton.Bones[i].Transform.localRotation);
-            // }
-            // OVRSkeleton rightSkeleton = rightHand.GetComponent<OVRSkeleton>();
-            // for (int i = 0; i < rightSkeleton.Bones.Count; i++)
-            // {
-            //     initialHandRotations.Add(rightSkeleton.Bones[i].Transform.name, rightSkeleton.Bones[i].Transform.localRotation);
-            // }
-            readMapping();
+            flag = true;
         }
-        foreach (KeyValuePair<GameObject, GameObject> pair in mapping)
+        if (!flag)
         {
-            if (pair.Key.transform.name.Contains("Root_M"))
+            OVRSkeleton rightSkeleton = rightHand.GetComponent<OVRSkeleton>();
+            if (rightSkeleton.Bones.Count > 10)
             {
-                pair.Key.transform.rotation = pair.Value.transform.rotation;
+                bool tempFlag = true;
+                for (int i = 0; i < rightSkeleton.Bones.Count; i++)
+                {
+                    if (controlledHandJoints.Contains(rightSkeleton.Bones[i].Transform.name))
+                    {
+                        Quaternion a = initialHandRotations[rightSkeleton.Bones[i].Transform.name];
+                        Quaternion b = rightSkeleton.Bones[i].Transform.localRotation;
+                        float angle = 0f;
+                        Vector3 axis = Vector3.zero;
+                        (a * Quaternion.Inverse(b)).ToAngleAxis(out angle, out axis);
+                        if (angle > 20)
+                        {
+                            tempFlag = false;
+                            break;
+                        }
+                    }
+                }
+                if (tempFlag)
+                {
+                    timer += Time.deltaTime;
+                    if (timer > 1f)
+                    {
+                        flag = true;
+                        timer = 0f;
+                        readMapping();
+                    }
+                }
+                else
+                {
+                    timer = 0f;
+                }
             }
-            else if (pair.Key.transform.name.Contains("_M"))
+        }
+        if (flag)
+        {
+            foreach (KeyValuePair<GameObject, GameObject> pair in mapping)
             {
-                pair.Key.transform.rotation = pair.Value.transform.rotation * Quaternion.Euler(180, 180, 0);
+                if (pair.Key.transform.name.Contains("Root_M"))
+                {
+                    pair.Key.transform.rotation = pair.Value.transform.rotation;
+                }
+                else if (pair.Key.transform.name.Contains("_M"))
+                {
+                    pair.Key.transform.rotation = pair.Value.transform.rotation * Quaternion.Euler(180, 180, 0);
+                }
+                else if (pair.Key.transform.name.Contains("nose"))
+                {
+                    Quaternion temp = pair.Value.transform.localRotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]);
+                    Quaternion initial = initialRotations[pair.Key.transform.name];
+                    pair.Key.transform.localRotation = Quaternion.Euler(temp.eulerAngles.x, temp.eulerAngles.y, -temp.eulerAngles.z) * initialRotations[pair.Key.transform.name];
+                }
+                else if (pair.Key.transform.name.Contains("Ear"))
+                {
+                    Quaternion temp = pair.Value.transform.localRotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]);
+                    Quaternion initial = initialRotations[pair.Key.transform.name];
+                    pair.Key.transform.localRotation = Quaternion.Euler(-temp.eulerAngles.x, temp.eulerAngles.y, temp.eulerAngles.z) * initialRotations[pair.Key.transform.name];
+                }
+                else if (pair.Key.transform.name.Contains("frontKnee") || pair.Key.transform.name.Contains("frontAnkle"))
+                {
+                    Quaternion temp = pair.Value.transform.localRotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]);
+                    Quaternion initial = initialRotations[pair.Key.transform.name];
+                    pair.Key.transform.localRotation = Quaternion.Euler(temp.eulerAngles.x, temp.eulerAngles.y, -temp.eulerAngles.z) * initialRotations[pair.Key.transform.name];
+                }
+                else
+                {
+                    // pair.Key.transform.rotation = pair.Value.transform.rotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]) * initialRotations[pair.Key.transform.name];
+                    pair.Key.transform.localRotation = pair.Value.transform.localRotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]) * initialRotations[pair.Key.transform.name];
+                }
+                continue;
+                // pair.Key.transform.rotation = Quaternion.Euler(-pair.Value.transform.rotation.eulerAngles.z, pair.Value.transform.rotation.eulerAngles.x, pair.Value.transform.rotation.eulerAngles.y);
             }
-            else if (pair.Key.transform.name.Contains("nose"))
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) && clusterPoseCnt < 5)
+        {
+            text.text = clusterPoseCnt.ToString();
+            clusterPoseRotations.Clear();
+            poseDeviations.Clear();
+            timer = 0f;
+            recordTimer = Time.time;
+            poseFlag = false;
+            foreach (KeyValuePair<string, List<float>> pair in clusterPoses[clusterPoseCnt])
             {
-                Quaternion temp = pair.Value.transform.localRotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]);
-                Quaternion initial = initialRotations[pair.Key.transform.name];
-                pair.Key.transform.localRotation = Quaternion.Euler(temp.eulerAngles.x, temp.eulerAngles.y, -temp.eulerAngles.z) * initialRotations[pair.Key.transform.name];
+                foreach (Transform g in anotherAvatar.transform.GetComponentsInChildren<Transform>())
+                {
+                    if (g.name == pair.Key)
+                    {
+                        g.localPosition = new Vector3(pair.Value[0], pair.Value[1], pair.Value[2]);
+                        g.localRotation = new Quaternion(pair.Value[3], pair.Value[4], pair.Value[5], pair.Value[6]);
+                        clusterPoseRotations.Add(g.name, g.localRotation);
+                        break;
+                    }
+                }
             }
-            else if (pair.Key.transform.name.Contains("Ear"))
+            clusterPoseCnt += 1;
+        }
+        if (!poseFlag && clusterPoseCnt > 0 && clusterPoseCnt < 6)
+        {
+            // after 1s, record the deviaiton of each joint (average)
+            // record the timer
+            bool tempFlag = true;
+            float tDevia = 0f;
+            foreach (Transform child in this.GetComponentsInChildren<Transform>())
             {
-                Quaternion temp = pair.Value.transform.localRotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]);
-                Quaternion initial = initialRotations[pair.Key.transform.name];
-                pair.Key.transform.localRotation = Quaternion.Euler(-temp.eulerAngles.x, temp.eulerAngles.y, temp.eulerAngles.z) * initialRotations[pair.Key.transform.name];
+                if (controlledJoints.Contains(child.name))
+                {
+                    Quaternion a = clusterPoseRotations[child.name];
+                    Quaternion b = child.localRotation;
+                    float angle = 0f;
+                    Vector3 axis = Vector3.zero;
+                    (a * Quaternion.Inverse(b)).ToAngleAxis(out angle, out axis);
+                    if (angle > 180)
+                    {
+                        angle -= 360;
+                    }
+                    angle = Mathf.Abs(angle);
+                    tDevia += angle;
+                }
             }
-            else if (pair.Key.transform.name.Contains("frontKnee") || pair.Key.transform.name.Contains("frontAnkle"))
+            if (tDevia / controlledJoints.Count > 15)
             {
-                Quaternion temp = pair.Value.transform.localRotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]);
-                Quaternion initial = initialRotations[pair.Key.transform.name];
-                pair.Key.transform.localRotation = Quaternion.Euler(temp.eulerAngles.x, temp.eulerAngles.y, -temp.eulerAngles.z) * initialRotations[pair.Key.transform.name];
+                // text.text = child.name + " " + angle.ToString();
+                tempFlag = false;
+            }
+            text.text = (tDevia / controlledJoints.Count).ToString();
+            if (tempFlag)
+            {
+                tDevia /= controlledJoints.Count;
+                poseDeviations.Add(tDevia);
+                timer += Time.deltaTime;
+                if (timer > 1f)
+                {
+                    float ttDevia = 0f;
+                    foreach (float t in poseDeviations)
+                    {
+                        ttDevia += t;
+                    }
+                    ttDevia /= poseDeviations.Count;
+                    float duration = Time.time - recordTimer;
+                    writer.WriteLine(clusterPoseCnt + " " + ttDevia + " " + duration.ToString());
+                    poseFlag = true;
+                    timer = 0f;
+                    poseDeviations.Clear();
+                    text.text = "Complete";
+                }
             }
             else
             {
-                // pair.Key.transform.rotation = pair.Value.transform.rotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]) * initialRotations[pair.Key.transform.name];
-                pair.Key.transform.localRotation = pair.Value.transform.localRotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]) * initialRotations[pair.Key.transform.name];
+                poseDeviations.Clear();
+                timer = 0f;
             }
-            continue;
-            // pair.Key.transform.rotation = Quaternion.Euler(-pair.Value.transform.rotation.eulerAngles.z, pair.Value.transform.rotation.eulerAngles.x, pair.Value.transform.rotation.eulerAngles.y);
-            
         }
+
         if (Input.GetKeyDown(KeyCode.Z))
         {
             int tempCnt = 0;
