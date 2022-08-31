996d7e7839a149bd16c1bcc49dab933ba59f960d
arsenalahm <lizhipeng0603@gmail.com>
2022-08-31 18:19:40 +0800

    Test kinect

--- Assets/ApplyMaping.cs
+++ Assets/ApplyMaping.cs
@@ -5,9 +5,15 @@ using System.IO;
 
 public class ApplyMaping : MonoBehaviour
 {
-    public GameObject leftHandIK;
-    private string mapping_file = "./Assets/spider_mapping_baseline.txt";
+    public GameObject avatar;
+    public GameObject user;
+    public GameObject anotherUser;
+
+    public string mapping_file = "./Assets/spider_mapping_baseline.txt";
     private Dictionary<string, Quaternion> initialRotations = new Dictionary<string, Quaternion>();
+    private Dictionary<string, Quaternion> initialUserRotations = new Dictionary<string, Quaternion>();
+
+    private string poseFile = "elephant_user_pose.txt";
 
     private Dictionary<GameObject, GameObject> mapping = new Dictionary<GameObject, GameObject>();
 
@@ -24,23 +30,58 @@ public class ApplyMaping : MonoBehaviour
             {
                 continue;
             }
-            GameObject ajoint = GameObject.Find(joints[0]);
-            GameObject hjoint = GameObject.Find(joints[1]);
-            initialRotations.Add(joints[0], ajoint.transform.rotation);
-            mapping.Add(ajoint, hjoint);
+            GameObject ajoint = null;
+            GameObject hjoint = null;
+            foreach (Transform g in avatar.transform.GetComponentsInChildren<Transform>())
+            {
+                if (g.name == joints[0])
+                {
+                    ajoint = g.gameObject;
+                    break;
+                }
+            }
+            foreach (Transform g in user.transform.GetComponentsInChildren<Transform>())
+            {
+                if (g.name == joints[1])
+                {
+                    hjoint = g.gameObject;
+                    break;
+                }
+            }
+            if (ajoint != null && hjoint != null)
+            {
+                initialRotations.Add(joints[0], ajoint.transform.localRotation);
+                mapping.Add(ajoint, hjoint);
+            }
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        StreamReader reader = new StreamReader(poseFile);
+        string[] content = reader.ReadToEnd().Split("\n");
+        foreach (string s in content)
+        {
+            string[] information = s.Split(" ");
+            foreach (Transform g in anotherUser.GetComponentsInChildren<Transform>())
+            {
+                if (g.name == information[0])
+                {
+                    g.position = new Vector3(float.Parse(information[1]), float.Parse(information[2]), float.Parse(information[3]));
+                    g.rotation = new Quaternion(float.Parse(information[4]), float.Parse(information[5]), float.Parse(information[6]), float.Parse(information[7]));
+                    initialUserRotations.Add(information[0], g.transform.localRotation);
+                    break;
+                }
+            }
+        }
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (UnityEngine.Input.GetKeyDown(KeyCode.A))
+        if (UnityEngine.Input.GetKeyDown(KeyCode.T))
         {
             readMapping();
         }
@@ -52,7 +93,7 @@ public class ApplyMaping : MonoBehaviour
             }
             else
             {
-                pair.Key.transform.rotation = pair.Value.transform.rotation * initialRotations[pair.Key.transform.name];
+                pair.Key.transform.localRotation = pair.Value.transform.localRotation * Quaternion.Inverse(initialUserRotations[pair.Value.transform.name]) * initialRotations[pair.Key.transform.name];
             }
         }
     }
---
+++ Assets/RecordUser.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class RecordUser : MonoBehaviour
+{
+    public GameObject user;
+
+    public string fileName = "elephant_user_pose.txt";
+    private StreamWriter writer;
+    
+    string ConvertTransformToString(Transform trans)
+    {
+        string temp = trans.name;
+        for (int i = 0; i < 3; i++)
+        {
+            temp += " " + trans.position[i];
+        }
+        for (int i = 0; i < 4; i++)
+        {
+            temp += " " + trans.rotation[i];
+        }
+        return temp;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        writer = new StreamWriter(fileName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            foreach (Transform g in user.GetComponentsInChildren<Transform>())
+            {
+                writer.WriteLine(ConvertTransformToString(g));
+            }
+        }
+        
+    }
+
+    void OnApplicationQuit()
+    {
+        writer.Close();
+    }
+}
