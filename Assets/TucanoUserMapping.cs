b1725f99f304c85003db06bf2719fbafc90503ab
Zhipeng <lizhipeng0603@gmail.com>
2022-11-12 22:55:15 -0500

    Add user's tucano

---
+++ Assets/TucanoUserMapping.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class TucanoUserMapping : MonoBehaviour
+{
+    private string mappingFile = "./Assets/tucano_mapping_user.txt";
+    private string poseFile = "tucano_hand_pose_user.txt";
+    
+    private Dictionary<GameObject, GameObject> mapping = new Dictionary<GameObject, GameObject>();
+    private Dictionary<string, Quaternion> initialRotations = new Dictionary<string, Quaternion>();
+    private Dictionary<string, Quaternion> initialHandRotations = new Dictionary<string, Quaternion>();
+
+    public GameObject initialLeftHand;
+    public GameObject initialRightHand;
+
+    public GameObject leftHand;
+    public GameObject rightHand;
+
+    private bool flag = false;
+
+    public GameObject avatar;
+
+    void readMapping()
+    {
+        StreamReader reader = new StreamReader(mappingFile);
+        string content = reader.ReadToEnd();
+        reader.Close();
+        string[] pairs = content.Split("\n");
+        foreach (string pair in pairs)
+        {
+            string[] joints = pair.Split(": ");
+            if (joints.Length != 2)
+            {
+                continue;
+            }
+            GameObject ajoint = null;
+            foreach (Transform g in avatar.transform.GetComponentsInChildren<Transform>())
+            {
+                if (g.name == joints[0])
+                {
+                    ajoint = g.gameObject;
+                }
+            }
+            GameObject hjoint = GameObject.Find(joints[1]);
+            if (ajoint != null && hjoint != null)
+            {
+                mapping.Add(ajoint, hjoint);
+                initialRotations.Add(ajoint.transform.name, ajoint.transform.localRotation);
+            }
+        }
+    }
+
+    string UppercaseFirst(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            return string.Empty;
+        }
+        return char.ToUpper(s[0]) + s.Substring(1);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StreamReader reader = new StreamReader(poseFile);
+        string[] content = reader.ReadToEnd().Split("\n");
+        foreach (string s in content)
+        {
+            string[] information = s.Split(" ");
+            if (s.Split("_")[0] == "Left")
+            {
+                foreach (Transform g in initialLeftHand.transform.GetComponentsInChildren<Transform>())
+                {
+                    if (!g.name.Contains("_") || g.name.Split("_")[0] != "b")
+                    {
+                        continue;
+                    }
+                    string temp = UppercaseFirst(g.name.Split("_")[2]);
+                    if (information[0].Contains(temp))
+                    {
+                        g.position = new Vector3(float.Parse(information[1]), float.Parse(information[2]), float.Parse(information[3]));
+                        g.rotation = new Quaternion(float.Parse(information[4]), float.Parse(information[5]), float.Parse(information[6]), float.Parse(information[7]));
+                        initialHandRotations.Add(information[0], g.transform.localRotation);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                foreach (Transform g in initialRightHand.transform.GetComponentsInChildren<Transform>())
+                {
+                    if (!g.name.Contains("_") || g.name.Split("_")[0] != "b")
+                    {
+                        continue;
+                    }
+                    string temp = UppercaseFirst(g.name.Split("_")[2]);
+                    if (information[0].Contains(temp))
+                    {
+                        g.position = new Vector3(float.Parse(information[1]), float.Parse(information[2]), float.Parse(information[3]));
+                        g.rotation = new Quaternion(float.Parse(information[4]), float.Parse(information[5]), float.Parse(information[6]), float.Parse(information[7]));
+                        initialHandRotations.Add(information[0], g.transform.localRotation);
+                        break;
+                    }
+                }
+            }
+        }
+        initialLeftHand.transform.position = new Vector3(0f, -0.1f, 0.2f);
+        initialRightHand.transform.position = new Vector3(0f, -0.1f, 0.2f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            readMapping();
+            flag = true;
+        }
+        if (flag)
+        {
+            foreach (KeyValuePair<GameObject, GameObject> pair in mapping)
+            {
+                if (pair.Value.transform.name.Contains("Forearm"))
+                {
+                    Quaternion temp = pair.Value.transform.rotation;
+                    pair.Key.transform.rotation = Quaternion.Euler(0, 30 + temp.eulerAngles.y, 180 + temp.eulerAngles.z);
+                }
+                else if (pair.Value.transform.name.Contains("Right_Middle"))
+                {
+                    Quaternion temp = pair.Value.transform.localRotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]);
+                    Quaternion initial = initialRotations[pair.Key.transform.name];
+                    pair.Key.transform.localRotation = Quaternion.Euler(temp.eulerAngles.z, 0, 0) * initial;
+                }
+                else if (pair.Value.transform.name.Contains("Left_Middle"))
+                {
+                    Quaternion temp = pair.Value.transform.localRotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]);
+                    Quaternion initial = initialRotations[pair.Key.transform.name];
+                    pair.Key.transform.localRotation = Quaternion.Euler(-temp.eulerAngles.z, 0, 0) * initial;
+                }
+                else if (pair.Value.transform.name.Contains("Right_Pinky"))
+                {
+                    Quaternion temp = pair.Value.transform.localRotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]);
+                    Quaternion initial = initialRotations[pair.Key.transform.name];
+                    pair.Key.transform.localRotation = Quaternion.Euler(0, 0, 30 + 2 * temp.eulerAngles.z) * initial;
+                }
+                else
+                {
+                    Quaternion temp = pair.Value.transform.localRotation * Quaternion.Inverse(initialHandRotations[pair.Value.transform.name]);
+                    Quaternion initial = initialRotations[pair.Key.transform.name];
+                    pair.Key.transform.localRotation = Quaternion.Euler(0, 0, -temp.eulerAngles.z) * initial;
+                }
+            }
+        }
+    }
+}
